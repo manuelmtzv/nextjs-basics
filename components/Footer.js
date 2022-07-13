25aeec6028f040012ce63850251f72810f31b80f
Manuel Martínez <manuel.mtzv816@gmail.com>
2022-07-13 13:56:41 -0500

    initial

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <footer>
+      Copyright 2022 Ninja List
+    </footer>
+  );
+}
+
+export default Footer; 
---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+const About = () => {
+  return ( 
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit rem asperiores nobis fugit, harum unde, repellendus vel soluta repellat, labore aliquid dolor enim eius sequi animi accusantium. Iure, vero doloremque!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit rem asperiores nobis fugit, harum unde, repellendus vel soluta repellat, labore aliquid dolor enim eius sequi animi accusantium. Iure, vero doloremque!
+        </p>
+      </div>
+    </>
+   );
+}
+ 
+export default About;
